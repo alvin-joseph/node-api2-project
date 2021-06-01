10fc5e6cfe897b04c5e29f6c1b532d284095b80e
alvin-joseph <aznman1234@gmail.com>
2021-06-01 18:22:47 -0500

    changed index/server files

--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 // implement your server here
+const express = require('express');
+const server = express();
+
 // require your posts router and connect it here
+const postRouter = require('./posts/posts-router');
+
+server.use(express.json());
+server.use('/api/posts', postRouter);
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Lambda Posts API</h>
+      <p>Welcome to the Lambda Posts API</p>
+    `);
+  });
+  
+  module.exports = server;
