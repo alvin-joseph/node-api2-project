bf117aa3ecda08426f65d3d19e4149aa4bc13d03
alvin-joseph <aznman1234@gmail.com>
2021-06-01 22:35:10 -0500

    finished mvp

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 // implement your server here
 const express = require('express');
+
 const server = express();
 
 // require your posts router and connect it here
 const postRouter = require('./posts/posts-router');
 
 server.use(express.json());
+
 server.use('/api/posts', postRouter);
 
 server.get('/', (req, res) => {
